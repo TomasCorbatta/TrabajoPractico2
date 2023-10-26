58016e448923375bbab09a741909404d4ce3e46c
TomasCorbatta <145059755+TomasCorbatta@users.noreply.github.com>
2023-10-26 18:43:39 -0300

    Add files via upload

---
+++ js/javas.js
@@ -0,0 +1,6 @@
+function info() {alert("Tomás Corbatta, Gmail: tcr6420@gmail.com");
+}
+function cambiarfondo() {
+        document.body.style.background="black";
+        document.body.style.color="white";
+        document.getElementById("footer").style.backgroundColor ="black";        }
